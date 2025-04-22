5159ca131421a89d2fc0cb43b69732f6de842d89
nerfall <nerfall10@gmail.com>
2025-04-22 16:18:58 +0300

    download default

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+// burger 
+
+    const burgerIcon = document.querySelector('.burger__icon');
+    const burgerMenu = document.querySelector('.burger__menu');
+
+    if (burgerIcon && burgerMenu) {
+        burgerIcon.addEventListener('click', () => {
+            burgerIcon.classList.toggle('active');
+            burgerMenu.classList.toggle('active');
+            document.body.style.overflow = burgerMenu.classList.contains('active') ? 'hidden' : '';
+        });
+
+        document.addEventListener('click', (event) => {
+            if (!burgerMenu.contains(event.target) && !burgerIcon.contains(event.target)) {
+                burgerIcon.classList.remove('active');
+                burgerMenu.classList.remove('active');
+                document.body.style.overflow = '';
+            }
+        });
+    }
+
+});
