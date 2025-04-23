7c2be771972d9d23d9f7b2099594c6d75b17eed3
Valleriiia <168529644+Valleriiia@users.noreply.github.com>
2025-04-23 10:55:56 +0300

    header adaptive

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,8 @@
     const header = document.querySelectorAll('.header_component')
     const headerComponent = ` <header class="header">
-            <div class="logo">
+            <a href="./" class="logo">
                 <img src="/assets/img/Logo.svg" alt="logo">
-            </div>
+            </a>
             <nav>
                 <div class="header-item">
                     <a href="#" class="header-link">Trading
@@ -10,9 +10,15 @@
                         <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
                     </svg></a>
                     <div class="header-item-dropdown">
-                        <a href="#" class="header-item-dropdown-link">Link 1</a>
-                        <a href="#" class="header-item-dropdown-link">Link 2</a>
-                        <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 1</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 2</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        </div>
                     </div>
                 </div>
                 <div class="header-item">
@@ -21,9 +27,15 @@
                         <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
                     </svg></a>
                     <div class="header-item-dropdown">
-                        <a href="#" class="header-item-dropdown-link">Link 1</a>
-                        <a href="#" class="header-item-dropdown-link">Link 2</a>
-                        <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 1</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 2</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        </div>
                     </div>
                 </div>
                 <div class="header-item">
@@ -32,9 +44,15 @@
                         <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
                     </svg></a>
                     <div class="header-item-dropdown">
-                        <a href="#" class="header-item-dropdown-link">Link 1</a>
-                        <a href="#" class="header-item-dropdown-link">Link 2</a>
-                        <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 1</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 2</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        </div>
                     </div>
                 </div>
                 <div class="header-item">
@@ -43,9 +61,15 @@
                         <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
                     </svg></a>
                     <div class="header-item-dropdown">
-                        <a href="#" class="header-item-dropdown-link">Link 1</a>
-                        <a href="#" class="header-item-dropdown-link">Link 2</a>
-                        <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 1</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 2</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        </div>
                     </div>
                 </div>
                 <div class="header-item">
@@ -54,9 +78,15 @@
                         <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
                     </svg></a>
                     <div class="header-item-dropdown">
-                        <a href="#" class="header-item-dropdown-link">Link 1</a>
-                        <a href="#" class="header-item-dropdown-link">Link 2</a>
-                        <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 1</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 2</a>
+                        </div>
+                        <div class="header-item-dropdown-item">
+                            <a href="#" class="header-item-dropdown-link">Link 3</a>
+                        </div>
                     </div>
                 </div>
                 <div class="header-item">
@@ -78,7 +108,137 @@
                     </div>
                 </div>
             </div>
+            <button class="burger-icon">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </button>
+            <div class="burger-menu">
+                <div class="burger-container">
+                    <nav>
+                        <div class="header-item">
+                            <a href="#" class="header-link"><p>Trading</p>
+                                <button class="header-item-btn">
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                    </svg>
+                                </button>
+                            </a>
+                            <div class="header-item-dropdown">
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 1</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 2</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-item">
+                            <a href="#" class="header-link"><p>Markets</p>
+                                <button class="header-item-btn">
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                    </svg>
+                                </button>
+                            </a>
+                            <div class="header-item-dropdown">
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 1</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 2</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-item">
+                            <a href="#" class="header-link"><p>Platform</p>
+                                <button class="header-item-btn">
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                    </svg>
+                                </button>
+                            </a>
+                            <div class="header-item-dropdown">
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 1</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 2</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-item">
+                            <a href="#" class="header-link"><p>Resources</p>
+                                <button class="header-item-btn">
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                    </svg>
+                                </button>
+                            </a>
+                            <div class="header-item-dropdown">
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 1</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 2</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-item">
+                            <a href="#" class="header-link"> <p>Company</p>
+                                <button class="header-item-btn">
+                                    <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                    </svg>
+                                </button>
+                            </a>
+                            <div class="header-item-dropdown">
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 1</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 2</a>
+                                </div>
+                                <div class="header-item-dropdown-item">
+                                    <a href="#" class="header-item-dropdown-link">Link 3</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-item">
+                            <a href="#" class="header-link">Partners</a>
+                        </div>
+                    </nav>
+                    <div class="header-btns">
+                        <a href="#" class="log-in">Log in</a>
+                        <a href="#" class="register">Register</a>
+                        <div class="header-language">
+                            <button class="language-btn">EN
+                                <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2842 8L8.5 10.6417L5.71579 8L5 8.67915L8.5 12L12 8.67915L11.2842 8Z" fill="#232323"/>
+                                </svg>
+                            </button>
+                            <div class="header-language-dropdown">
+                                <button class="language">EN</button>
+                                <button class="language">FR</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div>
         </header>
+
     `
     header.forEach( (el) => {
       return el.innerHTML = headerComponent
